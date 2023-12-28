f0ff65460010b62951bf55bfde4374f52434f5a8
Rohtash Lahry <rohtashlahry@gmail.com>
2023-12-28 18:47:40 +0530

    temepra-intial-setup

--- next.config.js
+++ next.config.js
@@ -2,3 +2,8 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    // other configurations...
+  };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,29 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import Cart from "@/Components/shopping-cart/Cart";
+import { HeroSection } from "../Components/HeroSection";
+import PriceGrid from "@/Components/price-packages/priceGrid";
+import Testimonials from "@/Components/reusable-components/testimonials";
+import TempAccordion from "@/Components/reusable-components/tempAccordion";
 
 export default function Home() {
+  const heading = "Empowering Businesses with Our Startup Agency";
+  const paraText = "Harness the boundless potential of Artificial Intelligence to transcend the realms of imagination and create mesmerizing masterpieces that time and space, captivating hearts and minds with beauty."
+  const ButtonTitle = "See All Projects";
+  const ButtonTitle2 = "Learn More";
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <HeroSection 
+        heading={heading}
+        paraText={paraText}
+        ButtonTitle={ButtonTitle}
+        ButtonTitle2={ButtonTitle2}
+      />
+      {/* <div style={{marginTop: 100}}>
+        <Cart />
+      </div> */}
+      <PriceGrid />
+      <Testimonials />
+      <TempAccordion />
+    </>
   )
 }
